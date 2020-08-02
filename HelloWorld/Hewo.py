a805b792e92845366ebcde1fd17b73165992e2ee
Red Tarantula <joaopedrothmann@hotmail.com>
2020-08-02 10:59:20 -0300

    Comments and data types

--- HelloWorld/Hewo.py
+++ HelloWorld/Hewo.py
@@ -1,1 +1,35 @@
-print("Hewo World! OWO")
+import os
+os.system('clear')
+
+'''
+Multi line commenting
+
+DATA TYPES
+- Strings
+- Numbers
+- Lists
+- Tuples: static list, it's faster
+- Dictionaries
+- Booleans
+
+
+'''
+
+# Single line commenting
+print("Hewo World! OWO") 
+
+full_name = "John"
+age = 21
+animals = ["Tarantula", "Rooster"] # list
+pokemon = ("Seviper", "Blaziken") # truple
+yoink = False
+
+fav_animal = {
+"John":"Rooster",
+"Todd":"Tiger",
+"Jax":"Frog"
+	
+}
+
+print(full_name)
+print(fav_animal["John"])
