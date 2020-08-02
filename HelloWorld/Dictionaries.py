02a83b47f71b48a19c5bf43e836156e43079e348
Red Tarantula <joaopedrothmann@hotmail.com>
2020-08-02 11:32:01 -0300

    Dictionaries

---
+++ HelloWorld/Dictionaries.py
@@ -0,0 +1,23 @@
+favorite_animals = {
+	"John":"Chicken",
+	"Joshua":"Dragon",
+	"Prime":"Renamon"
+}
+
+print(favorite_animals)
+print(favorite_animals["John"])
+
+del favorite_animals["John"]
+
+print(favorite_animals)
+
+favorite_animals.update({"Vyse":"Wolf"})
+
+favorite_animals["Prime"] = "Cat"
+
+print(favorite_animals)
+
+favorite_animals.update({"John":["Chicken","Bat","Tarantula"]})
+
+print(favorite_animals)
+print(favorite_animals["John"][1])
