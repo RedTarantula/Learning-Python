52dd25cbbf8b696e90ef082c2ff02192dceac722
Red Tarantula <joaopedrothmann@hotmail.com>
2020-08-02 11:14:05 -0300

    Math

--- HelloWorld/Hewo.py
+++ HelloWorld/Hewo.py
@@ -33,3 +33,32 @@ fav_animal = {
 
 print(full_name)
 print(fav_animal["John"])
+
+
+s = "Yoinking \"poopy\" haha very fucking \n FUNNY"
+print(s);
+
+s = "hello, my name is " + full_name;
+print(s);
+print(s.upper());
+print(s.lower());
+print(s.capitalize());
+print(s.title());
+print(s.swapcase());
+print(len(s)); # length of the string
+print(s[3])
+print(s[0:5]) # doesnt include 5th
+print(s.split(" "))
+print(s.split(" ")[2])
+
+
+i = 1
+f = 10.10
+
+print(i)
+print(float(i))
+print(f)
+print(int(f))
+
+print(5**5) # Power of
+print(9%3.5) # Returns remainder
